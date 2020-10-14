0e5c0b34db5121d334ebb11376329d8a48b41b76
Joseph <josephkim570@gmail.com>
2020-10-14 17:20:19 -0400

    Created a simple shipping form that reflects a similar style to the product form component. The checkout button simply console logs the form for now.

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  shipping = {}; 
 
   constructor() { }
+
+  placeOrder() {
+    console.log(this.shipping);
+  }  
 
   ngOnInit(): void {
   }
